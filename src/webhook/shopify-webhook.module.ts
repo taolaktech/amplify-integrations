f1ca57fddcd540ff7445aeff9e6b0cde31fe1572
taolak <taolak4orcl@gmail.com>
2025-12-06 22:16:59 -0600

    auht

--- src/webhook/shopify-webhook.module.ts
+++ src/webhook/shopify-webhook.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhookController } from './shopify-webhook.controller';
 import { ShopifyService } from '../shopify/shopify.service';
+import { ShopifyAuthService } from '../shopify/api/auth';
 
 @Module({
   controllers: [WebhookController],
-  providers: [ShopifyService],
+  providers: [ShopifyService, ShopifyAuthService],
 })
 export class ShopifyWebhookModule {}
