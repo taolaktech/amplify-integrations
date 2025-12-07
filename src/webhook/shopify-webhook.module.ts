27e3b33c3e613cc8665c4b8d15e3e01d9c2a71df
taolak <taolak4orcl@gmail.com>
2025-12-06 22:24:00 -0600

    graph module

--- src/webhook/shopify-webhook.module.ts
+++ src/webhook/shopify-webhook.module.ts
@@ -3,9 +3,15 @@ import { WebhookController } from './shopify-webhook.controller';
 import { ShopifyService } from '../shopify/shopify.service';
 import { ShopifyAuthService } from '../shopify/api/auth';
 import { ShopifyGraphqlAdminApi } from '../shopify/api/graphql-admin';
+import { ShopifyStoreFrontApi } from '../shopify/api/store-front-api';
 
 @Module({
   controllers: [WebhookController],
-  providers: [ShopifyService, ShopifyAuthService, ShopifyGraphqlAdminApi],
+  providers: [
+      ShopifyService,
+      ShopifyAuthService,
+      ShopifyStoreFrontApi,
+      ShopifyGraphqlAdminApi,
+    ],
 })
 export class ShopifyWebhookModule {}
