79dc2147558e1758f8fc7db248f3d5e14ff93da6
taolak <taolak4orcl@gmail.com>
2025-12-06 22:22:12 -0600

    graph module

--- src/webhook/shopify-webhook.module.ts
+++ src/webhook/shopify-webhook.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WebhookController } from './shopify-webhook.controller';
 import { ShopifyService } from '../shopify/shopify.service';
 import { ShopifyAuthService } from '../shopify/api/auth';
+import { ShopifyGraphqlAdminApi } from '../shopify/api/graphql-admin';
 
 @Module({
   controllers: [WebhookController],
-  providers: [ShopifyService, ShopifyAuthService],
+  providers: [ShopifyService, ShopifyAuthService, ShopifyGraphqlAdminApi],
 })
 export class ShopifyWebhookModule {}
